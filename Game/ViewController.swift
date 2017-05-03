ac50e5c61cc60df3349d81720c4d8ba27e89d658
Артем Сулейманов <artemsulejmanov@macbook-pro-artem.local>
2017-05-03 09:56:23 +0300

    Initial Commit

---
+++ Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Артем Сулейманов on 03.05.17.
+//  Copyright © 2017 Артем Сулейманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
